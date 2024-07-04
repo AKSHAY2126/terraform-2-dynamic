9fe76cbc0e8a64f5674df74eb887cb4d254ca7cd
AKSHAY2126 <akshaysingh5000@gmail.com>
2024-07-04 18:38:13 +0530

    infra creation-VNet & backend state file

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "myrg-1"
-    storage_account_name  = "mytfstateaccount2121"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name   = "myrg-1"
+#     storage_account_name  = "mytfstateaccount2121"
+#     container_name        = "tfstate"
+#     key                   = "terraform.tfstate"
+#   }
+# }
 
 # provider "azurerm" {
 #   features {}
